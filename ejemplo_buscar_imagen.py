302fa5a6ed30dec0fa28f8ebaf151cba3c60e660
ruben perez ibañez <rubenpeib@gmail.com>
2022-03-08 21:09:04 +0100

    primer git

---
+++ ejemplo_buscar_imagen.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from pynput.mouse import Button, Controller
+import time
+from screen_search import Search
+
+mouse = Controller()
+def click_raton_posicion (x,y):
+    mouse.position = (x, y)
+    print('Now we have moved it to {0}'.format(
+        mouse.position))
+    # Press and release
+    mouse.press(Button.left)
+    mouse.release(Button.left)
+    time.sleep(1)
+
+
+
+def imagen(imagen):
+    search = Search(imagen)
+    pos = search.imagesearch()
+    if pos[0] == -1:
+        return False
+    else:
+        return pos
+
+
+time.sleep(3)
+path = 'img/'
+coordenadas = imagen(path+ 'eleconomista.png')
+#si encuentro
+if coordenadas!= False:
+        click_raton_posicion (coordenadas[0], coordenadas[1])
+
+
+
